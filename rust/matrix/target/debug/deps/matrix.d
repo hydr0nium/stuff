6b1689b5a8fad17bd0103f9a732a07526ccf344a
Sol <37932436+hydr0nium@users.noreply.github.com>
2025-07-18 01:02:42 +0200

    Delete rust/matrix/target directory

--- rust/matrix/target/debug/deps/matrix.d
+++
@@ -1,6 +0,0 @@
-C:\Users\demia\Documents\git\stuff\rust\matrix\target\debug\deps\matrix.d: src\main.rs src\matrix.rs
-
-C:\Users\demia\Documents\git\stuff\rust\matrix\target\debug\deps\matrix.exe: src\main.rs src\matrix.rs
-
-src\main.rs:
-src\matrix.rs:
--- rust/matrix/target/debug/matrix.d
+++
@@ -1,1 +0,0 @@
-C:\Users\demia\Documents\git\stuff\rust\matrix\target\debug\matrix.exe: C:\Users\demia\Documents\git\stuff\rust\matrix\src\main.rs C:\Users\demia\Documents\git\stuff\rust\matrix\src\matrix.rs
